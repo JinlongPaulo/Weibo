1b41c7d6f7646632ea846433ea7ad8027fee0954
Paulo <306244907@qq.com>
2017-09-29 18:01:02 +0800

    文件结构部署，设置启动文件

--- JLSina/JLSina/AppDelegate.swift
+++ JLSina/JLSina/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        window = UIWindow()
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = JLMainViewController()
+        window?.makeKeyAndVisible()
         
         return true
     }
