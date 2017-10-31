4112758f63e33f89f04fe85a09d3d4209bb50308
Paulo <306244907@qq.com>
2017-10-31 15:16:33 +0800

    拓展测试控制器，不停的push

--- JLSina/JLSina/Classes/View(视图和控制器)/Main/测试/JLDemoViewController.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Main/测试/JLDemoViewController.swift
@@ -10,26 +10,24 @@ import UIKit
 
 class JLDemoViewController: JLBaseViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //设置标题
+        title = "第\(navigationController?.childViewControllers.count ?? 0)个"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //继续push新的控制
+    @objc private func showNext() {
+        let vc = JLDemoViewController()
+        navigationController?.pushViewController(vc, animated: true)
+        
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension JLDemoViewController {
+    override func setupUI() {
+        super.setupUI()
+        //设置右侧控制器
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "下一个", style: .plain, target: self, action: #selector(showNext))
     }
-    */
-
 }
