66bde309be3684a85beb229ef164ed9ef13dedc6
Paulo <306244907@qq.com>
2017-10-31 14:58:53 +0800

    添加测试控制器，push显示

--- JLSina/JLSina/Classes/View(视图和控制器)/Home/JLHomeViewController.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Home/JLHomeViewController.swift
@@ -15,21 +15,24 @@ class JLHomeViewController: JLBaseViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //显示好友
+    @objc private func showFriends() {
+        print(#function)
+        let vc = JLDemoViewController()
+        vc.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(vc, animated: true)
+        
     }
-    */
 
+}
+
+//MARK: - 设置界面
+extension JLHomeViewController {
+    //重写父类方法
+    override func setupUI() {
+         super.setupUI()
+        //设置导航栏按钮
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "好友", style: .plain, target: self, action: #selector(showFriends))
+    }
 }
