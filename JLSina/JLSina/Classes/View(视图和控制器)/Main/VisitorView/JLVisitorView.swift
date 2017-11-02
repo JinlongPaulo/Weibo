a9184cd1b51b6fd2b5f8d4f329cb72443126bab0
Paulo <306244907@qq.com>
2017-11-02 15:07:35 +0800

    懒加载访客视图控件&extension和懒加载类型的细节

--- JLSina/JLSina/Classes/View(视图和控制器)/Main/VisitorView/JLVisitorView.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Main/VisitorView/JLVisitorView.swift
@@ -19,6 +19,21 @@ class JLVisitorView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //MARK: - 私有控件
+    //懒加载属性只有调用UIKit 控件的指定构造函数，其他都需要使用类型
+    // 图像视图
+    private lazy var iconView: UIImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
+    //小房子
+    private lazy var houseIconView: UIImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_house"))
+    //提示标签
+    private lazy var tipLabel: UILabel = UILabel.cz_label(
+        withText: "关注一些人，回这里看看有什么惊喜",
+        fontSize: 14,
+        color: UIColor.darkGray)
+    //注册按钮
+    private lazy var registerBtn: UIButton = UIButton.cz_textButton("注册", fontSize: 16, normalColor: UIColor.orange, highlightedColor: UIColor.black, backgroundImageName: "common_button_white_disable")
+    //登录按钮
+    private lazy var loginBtn: UIButton = UIButton.cz_textButton("登录", fontSize: 16, normalColor: UIColor.darkGray, highlightedColor: UIColor.black, backgroundImageName: "common_button_white_disable")
 }
 
 //MARK: - 设置界面
