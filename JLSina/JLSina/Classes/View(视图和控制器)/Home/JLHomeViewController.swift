726e7ce436f50c6c94cbaa422ad46eb16dee16d5
Paulo <306244907@qq.com>
2017-11-02 10:25:23 +0800

    首页绑定假数据

--- JLSina/JLSina/Classes/View(视图和控制器)/Home/JLHomeViewController.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Home/JLHomeViewController.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
+//定义全局常量，尽量使用private修饰
+private let cellId = "cellId"
+
 class JLHomeViewController: JLBaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    //微博数据源数组
+    private lazy var statusList = [String]()
+    
+    //加载数据
+    override func loadData() {
+        for i in 0..<10 {
+            statusList.insert(i.description, at: 0)
+        }
     }
     
     //显示好友
@@ -27,6 +34,24 @@ class JLHomeViewController: JLBaseViewController {
 
 }
 
+//MARK: - 表格数据源方法,具体的数据源方法实现，不需要super
+extension JLHomeViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return statusList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //1,取cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        //2,设置cell
+        cell.textLabel?.text = statusList[indexPath.row]
+        //3,返回cell
+        return cell
+    }
+}
+
 //MARK: - 设置界面
 extension JLHomeViewController {
     //重写父类方法
@@ -35,5 +60,7 @@ extension JLHomeViewController {
         //设置导航栏按钮
         navItem.leftBarButtonItem = UIBarButtonItem(title: "好友", target: self, action: #selector(showFriends))
 
+        //注册原型cell
+        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
 }
