d5f856c9de97f5bf71d4d72682998f5a5e77810c
Paulo <306244907@qq.com>
2017-11-09 16:17:24 +0800

    加载授权页面

--- JLSina/JLSina/Classes/View(视图和控制器)/Main/OAuth/JLWBOAuthViewController.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Main/OAuth/JLWBOAuthViewController.swift
@@ -22,6 +22,25 @@ class JLWBOAuthViewController: UIViewController {
         //导航栏按钮
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回" , target: self, action: #selector(close), isBack: true)
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //加载授权界面
+        let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(WBAppKey)&redirect_uri=\(WBRedirectURI)"
+        
+        //1>URL确定要访问的资源
+        guard let url = URL(string: urlString) else {
+                return
+        }
+        
+        //2>建立请求
+        let request = URLRequest(url: url)
+        
+        //3>加载请求
+        webView.loadRequest(request)
+        
+    }
 
     //MARK: - 监听方法
     @objc private func close() {
