d3858320b6d6db5805ac5837d5d8363d508bcf0c
Paulo <306244907@qq.com>
2017-11-10 22:40:40 +0800

    从磁盘加载用户账户信息并且设置模型

--- JLSina/JLSina/Classes/Tools(工具)/Network/Model/JLUserAccount.swift
+++ JLSina/JLSina/Classes/Tools(工具)/Network/Model/JLUserAccount.swift
@@ -35,8 +35,18 @@ class JLUserAccount: NSObject {
         super.init()
         
         //从磁盘加载保存的文件 ->字典
+        //1>加载磁盘文件到二进制，如果失败直接返回
+        guard let path = accountFile.cz_appendDocumentDir() ,
+              let data = NSData(contentsOfFile: path) ,
+              let dict = try? JSONSerialization.jsonObject(with: data as Data, options: []) as? [String: AnyObject]
+        else {
+                return
+        }
         
-        //使用字典设置属性值
+        //2>使用字典设置属性值
+        yy_modelSet(with: dict ?? [:])
+        
+        print("从沙盒加载信息\(self)")
     }
     
     override var description: String {
