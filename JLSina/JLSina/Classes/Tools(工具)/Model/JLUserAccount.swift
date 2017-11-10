20d1bc0a90b2a2e946223c96a8b7cac9a427bb07
Paulo <306244907@qq.com>
2017-11-10 12:01:31 +0800

    数据存储方式的回忆

--- JLSina/JLSina/Classes/Tools(工具)/Model/JLUserAccount.swift
+++ JLSina/JLSina/Classes/Tools(工具)/Model/JLUserAccount.swift
@@ -32,4 +32,14 @@ class JLUserAccount: NSObject {
     override var description: String {
         return yy_modelDescription()
     }
+    
+    /*
+     1.偏好设置(存小的) - Xcode 8.beta 无效
+     2.沙盒-归档/plist/json
+     3.数据库(FMDB/CoreData)
+     4.钥匙串(小/自动加密-需要使用框架SSKeychina)
+     */
+    func saveAccount() {
+        
+    }
 }
