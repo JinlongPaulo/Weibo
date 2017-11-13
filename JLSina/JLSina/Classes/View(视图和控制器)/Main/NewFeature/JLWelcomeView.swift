858708994510a992e4ad8f6890cf35e7879b45eb
Paulo <306244907@qq.com>
2017-11-13 14:01:58 +0800

    欢迎界面自动布局动画实现

--- JLSina/JLSina/Classes/View(视图和控制器)/Main/NewFeature/JLWelcomeView.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Main/NewFeature/JLWelcomeView.swift
@@ -11,6 +11,12 @@ import UIKit
 //欢迎视图
 class JLWelcomeView: UIView {
     
+    @IBOutlet weak var iconView: UIImageView!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var bottomCons: NSLayoutConstraint!
+    
     class func welcomeView()-> JLWelcomeView {
         let nib = UINib(nibName: "JLWelcomeView", bundle: nil)
         
@@ -21,5 +27,32 @@ class JLWelcomeView: UIView {
         
         return v
     }
+    
+    //视图被添加到 window上，表示视图已经显示
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        
+        //视图是使用自动布局来设置的，只是设置了约束
+        //当视图被添加到窗口上时，根据父视图的大小，计算约束值，更新控件位置
+        // -layoutIfNeeded 会直接按照当前的约束，直接更新控件位置
+        // - 执行之后控件所在位置，就是xib中布局约束指定的位置
+        self.layoutIfNeeded()
+        
+        bottomCons.constant = bounds.size.height - 200
+        
+        //如果控件们的 frame 还没有计算好，所有的约束会一起动画
+        UIView.animate(withDuration: 2.0, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: [], animations: {
+            //更新约束
+            self.layoutIfNeeded()
+        }) { (_) in
+            
+        }
+    }
+
+    //自动布局系统更新完成约束后，会调用此方法
+    //通常是对子视图布局进行修改
+//    override func layoutSubviews() {
+//
+//    }
     
 }
