3201b8cd611ff4df81126ee98bf6b8f12cd00f96
Paulo <306244907@qq.com>
2017-11-13 11:23:25 +0800

    准备欢迎视图和新特性视图

---
+++ JLSina/JLSina/Classes/View(视图和控制器)/Main/NewFeature/JLNewFeature.swift
@@ -0,0 +1,24 @@
+//
+//  JLNewFeature.swift
+//  JLSina
+//
+//  Created by 盘赢 on 2017/11/13.
+//  Copyright © 2017年 JinLong. All rights reserved.
+//
+
+import UIKit
+
+class JLNewFeature: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.orange
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+}
---
+++ JLSina/JLSina/Classes/View(视图和控制器)/Main/NewFeature/JLWelcomeView.swift
@@ -0,0 +1,24 @@
+//
+//  JLWelcomeView.swift
+//  JLSina
+//
+//  Created by 盘赢 on 2017/11/13.
+//  Copyright © 2017年 JinLong. All rights reserved.
+//
+
+import UIKit
+
+//欢迎视图
+class JLWelcomeView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
