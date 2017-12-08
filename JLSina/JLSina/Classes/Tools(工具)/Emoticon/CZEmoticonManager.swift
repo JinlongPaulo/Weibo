db60113be92617efca1a98eff1c19ff966185ccf
Paulo <306244907@qq.com>
2017-12-08 18:00:43 +0800

    使用 FFLabel 设置微博文字和转发文字的自定义类，修改属性文本颜色

--- JLSina/JLSina/Classes/Tools(工具)/Emoticon/CZEmoticonManager.swift
+++ JLSina/JLSina/Classes/Tools(工具)/Emoticon/CZEmoticonManager.swift
@@ -67,7 +67,9 @@ extension CZEmoticonManager {
         }
         
         //4,*****统一设置一遍字符串的属性，除了需要设置字体，还需要设置'颜色'
-        attrString.addAttributes([NSAttributedStringKey.font: font], range: NSRange(location: 0, length: attrString.length))
+        attrString.addAttributes([NSAttributedStringKey.font: font ,
+                                  NSAttributedStringKey.foregroundColor: UIColor.darkGray],
+                                 range: NSRange(location: 0, length: attrString.length))
         return attrString
     }
     
