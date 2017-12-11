304f3d5cb7e4d90c40ffbb03b1a1c322f3630968
Paulo <306244907@qq.com>
2017-12-11 15:04:23 +0800

    添加撰写素材

--- JLSina/JLSina/Classes/View(视图和控制器)/Compose撰写微博/JLComposeViewController.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Compose撰写微博/JLComposeViewController.swift
@@ -24,7 +24,7 @@ class JLComposeViewController: UIViewController {
 
         view.backgroundColor = UIColor.cz_random();
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "退出", target: self, action: #selector(close))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "关闭", target: self, action: #selector(close))
     }
 
     @objc private func close() {
@@ -33,18 +33,8 @@ class JLComposeViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
