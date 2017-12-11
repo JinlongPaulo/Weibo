f0c4bd1e2feb9d8ae2d761996a82daaa096645ab
Paulo <306244907@qq.com>
2017-12-11 15:00:41 +0800

    撰写控制器简单布局

--- JLSina/JLSina/Classes/View(视图和控制器)/Compose撰写微博/JLComposeViewController.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Compose撰写微博/JLComposeViewController.swift
@@ -9,19 +9,28 @@
 import UIKit
 
 //撰写微博控制器
+/*
+ 加载视图控制器的时候，如果XIB和控制器重名，默认的构造函数会优先加载XIB
+ */
 class JLComposeViewController: UIViewController {
 
+    //文本编辑视图
+    @IBOutlet weak var textView: UITextView!
+    //底部工具栏
+    @IBOutlet weak var toolBar: UIToolbar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor.cz_random();
         
-        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "退出", target: self, action: #selector(close))
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-         self.dismiss(animated: true, completion: nil)
+    @objc private func close() {
+        dismiss(animated: true, completion: nil)
     }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- JLSina/JLSina/Classes/View(视图和控制器)/Home/JLWebViewController.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Home/JLWebViewController.swift
@@ -24,7 +24,6 @@ class JLWebViewController: JLBaseViewController {
             webView.loadRequest(URLRequest(url: url))
         }
     }
-    
 }
 
 extension JLWebViewController {
