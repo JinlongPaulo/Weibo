5f59561b479ee798c0ab46feb0c59a6c8b173ed6
Paulo <306244907@qq.com>
2017-12-13 13:20:08 +0800

    利用通知调整 toolbar 底部约束

--- JLSina/JLSina/Classes/View(视图和控制器)/Compose撰写微博/JLComposeViewController.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Compose撰写微博/JLComposeViewController.swift
@@ -23,13 +23,47 @@ class JLComposeViewController: UIViewController {
     @IBOutlet var sendButton: UIButton!
     
     //标题标签 - 换行热键 option + 回车
+    //逐行选中文本并且设置属性
+    //如果要想调整行间距，增加空行，设置空行的字体，lineHeight
     @IBOutlet var titleLabel: UILabel!
     
+    //工具栏底部约束
+    @IBOutlet weak var toolBarBottomCons: NSLayoutConstraint!
     //MARK: - 视图生命周期
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
+        
+        //监听键盘通知 - UIWindow.h
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardChanged),
+                                               name: NSNotification.Name.UIKeyboardWillChangeFrame,
+                                               object: nil)
+    }
+    
+    //MARK: - 键盘监听
+    @objc private func keyboardChanged(n: Notification) {
+//        print(n.userInfo)
+        //1,目标rect
+        guard let rect = (n.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+              let duration = (n.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue else {
+            return
+        }
+        
+       
+        
+        
+        //2,设置底部约束的高度
+        let offset = view.bounds.height - rect.origin.y
+        
+        //3,更新底部约束
+        toolBarBottomCons.constant = offset
+        
+        //动画更新约束
+        UIView.animate(withDuration: duration) {
+            self.view.layoutIfNeeded()
+        }
     }
 
     @objc private func close() {
@@ -48,6 +82,47 @@ private extension JLComposeViewController {
     func setupUI() {
         view.backgroundColor = UIColor.white
         setupNavtionBar()
+        setupToolBar()
+    }
+    
+    ///设置工具栏
+    func setupToolBar() {
+        let itemSettings = [
+            ["imageName": "compose_toolbar_picture"],
+            ["imageName": "compose_mentionbutton_background"],
+            ["imageName": "compose_trendbutton_background"],
+            ["imageName": "compose_emoticonbutton_background", "actionName": "emoticonKeyboard"],
+            ["imageName": "compose_add_background"]
+        ]
+        
+        //遍历数组
+        var items = [UIBarButtonItem]()
+        
+        for s in itemSettings {
+            guard let imageName = s["imageName"] else {
+                continue
+            }
+            
+            let image = UIImage.init(named: imageName)
+            let imageHL = UIImage.init(named: imageName + "_highlighted")
+            
+            
+            let btn = UIButton()
+            btn.setImage(image, for: [])
+            btn.setImage(imageHL, for: .highlighted)
+            
+            btn.sizeToFit()
+            
+            //追加按钮
+            items .append(UIBarButtonItem(customView: btn))
+            
+            //追加弹簧
+            items.append(UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil))
+        }
+        
+        // 删除末尾弹簧
+        items.removeLast()
+        toolBar.items = items
     }
     
     //设置导航栏
