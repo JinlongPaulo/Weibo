3382833105e9bd64be1d9277f8c2e734c4f5c486
Paulo <306244907@qq.com>
2017-12-13 13:43:13 +0800

    自定义文本视图，添加占位 label

--- JLSina/JLSina/Classes/View(视图和控制器)/Compose撰写微博/View/JLComposeTextView.swift
+++ JLSina/JLSina/Classes/View(视图和控制器)/Compose撰写微博/View/JLComposeTextView.swift
@@ -11,4 +11,24 @@ import UIKit
 //撰写微博的文本视图
 class JLComposeTextView: UITextView {
 
+    //占位标签
+    private lazy var placeholderLabel = UILabel()
+    
+    override func awakeFromNib() {
+        setupUI()
+    }
+}
+
+private extension JLComposeTextView {
+    func setupUI() {
+        
+        //1,设置占位标签
+        placeholderLabel.text = "分享新鲜事..."
+        placeholderLabel.font = self.font
+        placeholderLabel.textColor = UIColor.lightGray
+        placeholderLabel.frame.origin = CGPoint(x: 5 , y: 8)
+        
+        placeholderLabel.sizeToFit()
+        addSubview(placeholderLabel)
+    }
 }
