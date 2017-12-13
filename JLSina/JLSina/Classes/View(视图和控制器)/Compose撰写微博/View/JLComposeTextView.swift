0480086d3f2348d8423afd40df467ecec5e57f92
Paulo <306244907@qq.com>
2017-12-13 13:34:07 +0800

    撰写微博的文本视图

---
+++ JLSina/JLSina/Classes/View(视图和控制器)/Compose撰写微博/View/JLComposeTextView.swift
@@ -0,0 +1,14 @@
+//
+//  JLComposeTextView.swift
+//  JLSina
+//
+//  Created by 盘赢 on 2017/12/13.
+//  Copyright © 2017年 JinLong. All rights reserved.
+//
+
+import UIKit
+
+//撰写微博的文本视图
+class JLComposeTextView: UITextView {
+
+}
