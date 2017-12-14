bd97c6d640b9b9c3bc7c9bcbb81824e97c7fe597
Paulo <306244907@qq.com>
2017-12-14 10:05:08 +0800

    添加工具栏按钮／以及简单的布局

--- JLSina/JLSina/Classes/Tools(工具)/Emoticon/View/CZEmoticonToolBar.swift
+++ JLSina/JLSina/Classes/Tools(工具)/Emoticon/View/CZEmoticonToolBar.swift
@@ -8,14 +8,49 @@
 
 import UIKit
 
+//表情键盘底部工具栏
 class CZEmoticonToolBar: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        setupUI()
     }
-    */
+    
+    override func layoutSubviews() {
+        //布局所有按钮
+        let count = subviews.count
+        let w = bounds.width / CGFloat(count)
+        
+        let rect = CGRect(x: 0, y: 0, width: w, height: bounds.height)
+        
+        
+        for (i, btn) in subviews.enumerated() {
+            btn.frame = rect.offsetBy(dx: CGFloat(i) * w, dy: 0)
+        }
+        
+        
+    }
+}
 
+private extension CZEmoticonToolBar {
+    
+    func setupUI() {
+        //0,获取表情包管理单例
+        let manager = CZEmoticonManager.shared
+        //1,从表情包的分组名称->设置按钮
+        for p in manager.packages {
+            //1,实例化按钮
+            let btn = UIButton()
+            
+            //2,设置按钮
+            btn.setTitle(p.groupName, for: [])
+            btn.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+            btn.setTitleColor(UIColor.white, for: [])
+            btn.setTitleColor(UIColor.darkGray, for: .highlighted)
+            btn.setTitleColor(UIColor.darkGray, for: .selected)
+            
+            btn.sizeToFit()
+            //3,添加按钮
+            addSubview(btn)
+        }
+    }
 }
