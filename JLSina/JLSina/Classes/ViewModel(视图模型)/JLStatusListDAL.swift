0587b1bba4dd326a241254db39fa95ab0171c280
Paulo <306244907@qq.com>
2017-12-19 15:18:38 +0800

    新增数据访问层，确定数据缓存思路

---
+++ JLSina/JLSina/Classes/ViewModel(视图模型)/JLStatusListDAL.swift
@@ -0,0 +1,16 @@
+//
+//  JLStatusListDAL.swift
+//  JLSina
+//
+//  Created by 盘赢 on 2017/12/19.
+//  Copyright © 2017年 JinLong. All rights reserved.
+//
+
+import Foundation
+
+/// DAL: - Data Access Layer 数据访问层
+/// 使命： 负责处理数据库和网络数据，给ListViewModel 返回微博的 [字典数组]
+/// 在调整系统的时候，尽量做最小化的调整
+class JLStatusListDAL {
+    
+}
